2b5e1845c06d978ff28414428bc1f3230cee6909
Yasin <ebrahimiy07@gmail.com>
2024-11-06 17:33:40 +0330

    costume forms

--- accounts/models.py
+++ accounts/models.py
@@ -1,9 +1,9 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, AbstractUser
+from django.contrib.auth.models import AbstractBaseUser
 from .managers import UserManager
 
 
-class User(AbstractUser):
+class User(AbstractBaseUser):
     email = models.EmailField(max_length=255, unique=True)
     phone_number = models.CharField(max_length=11, unique=True)
     full_name = models.CharField()
